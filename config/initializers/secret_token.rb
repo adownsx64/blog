44963509132dc841553a758756e8f76dbbb9733d
Andrew J. Downs <adowns@unm.edu>
2011-10-05 21:35:17 -0600

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd776d4a70f9d5f73a83e1c4520aae95d1a0e90368f25b414298d073b64eab16f8bd7919f232751e6f3bbf84222a205b7bbe0b0cd4a4f3d716f3af9024917f3ae'
